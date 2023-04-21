238577010ab75696911f0839eb0db594a79b88e5
Thomas deBlaquiere <tdwebdesignmsu@yahoo.com>
2023-04-21 11:38:23 -0400

    Implemented Login, Logout views w/ conditional nav

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
-app_name = 'account'
+#app_name = 'account'
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # previous login url
+    #path('login/', views.user_login, name='login'),
+    # Login / Logout urls
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
 ]
