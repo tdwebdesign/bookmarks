9e92ee846290387cc45ddafce28104d9ba2e3cb9
Thomas deBlaquiere <tdwebdesignmsu@yahoo.com>
2023-04-21 11:56:02 -0400

    Implemented password change

--- account/urls.py
+++ account/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
     # Login / Logout urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Change password urls
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
     path('', views.dashboard, name='dashboard'),
 ]
