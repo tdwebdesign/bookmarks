b30caf635adfffcf1e1f606ee36f37b4ba3aed2c
Thomas deBlaquiere <tdwebdesignmsu@yahoo.com>
2023-04-21 13:00:40 -0400

    Finished password reset, commented out, using default

--- account/urls.py
+++ account/urls.py
@@ -1,19 +1,26 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
 #app_name = 'account'
 
 urlpatterns = [
-    # previous login url
-    #path('login/', views.user_login, name='login'),
-    # Login / Logout urls
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # # previous login url
+    # path('login/', views.user_login, name='login'),
+    # # Login / Logout urls
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    # Change password urls
-    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # # Change password urls
+    # path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
+    # # Reset password urls
+    # path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    
+    path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
 ]
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -81,6 +81,9 @@ DATABASES = {
     }
 }
 
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
