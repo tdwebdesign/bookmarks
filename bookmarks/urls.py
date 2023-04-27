91955dbdb58cd5661c03fcb4a7031e344cc31e95
Thomas deBlaquiere <tdwebdesignmsu@yahoo.com>
2023-04-27 12:07:27 -0400

    Added Bookmark functionality with javascript bookmarklet

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
+    path('images/', include('images.urls', namespace='images')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
